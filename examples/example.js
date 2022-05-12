2e281a7fd43e6338f2365f9502c841c95667dd39
Carter Roeser <carter@cdgtech.one>
2022-05-11 18:49:32 -0700

    Update example.js

--- examples/example.js
+++ examples/example.js
@@ -2,7 +2,8 @@ const WomboDream = require('dream-api');
 
 async function main() {
     await WomboDream.printStyles();
-    console.log(await WomboDream.generateImage(1, "cat"));
+    let styles = await WomboDream.getStyles();
+    console.log(await WomboDream.generateImage(1, "dog"));
 }
 
 main();
