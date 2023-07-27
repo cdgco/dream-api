fc5fa3a5fdb2f5861110ab25ae400a7203ec7ac8
Carter Roeser <carter@cdgtech.one>
2023-07-26 19:55:54 -0700

    Update test.js

--- test/test.js
+++ test/test.js
@@ -11,15 +11,24 @@ describe('dream-api tests', async function() {
         this.image = null;
     });
     it('getStyles() responds with array of styles', async function() {
+        console.log("Running Test 1");
         const styles = await WomboDream.getStyles();
         styles.should.be.an('array');
+        console.log(styles)
+        console.log("Finished Test 1");
     });
     it('generateImage() (prompt only) responds with image object', async function() {
+        console.log("Running Test 2");
         this.image = await WomboDream.generateImage(1, "dog", this.token, null, "MEDIUM", null, null);
         this.image.should.have.property('result');
+        console.log(this.image)
+        console.log("Finished Test 2");
     });
     it('generateImage() (prompt + upload) responds with image object', async function() {
+        console.log("Running Test 3");
         this.image = await WomboDream.generateImage(1, "dog", this.token,'./test/test.jpg', "MEDIUM", null, null);
         this.image.should.have.property('result');
+        console.log(this.image)
+        console.log("Finished Test 3");
     });
 });
